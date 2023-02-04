a0b3ab8c7148ceb53a378c6b4516f00c6ab8e861
MattsFace <spahmatthew@gmail.com>
2023-02-04 15:20:14 -0800

    Adding ansible directory

---
+++ ansible/build_inventory.py
@@ -0,0 +1,31 @@
+import boto3
+import json
+import re
+
+ec2 = boto3.client('ec2')
+response = ec2.describe_instances()
+hosts = {}
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+
+        public_ip = instance['PublicIpAddress']
+
+        for tag in instance['Tags']:
+            if tag['Key'] == "Name":
+
+                host_name = tag['Value']
+                hosts[host_name] = public_ip
+
+
+with open("inventory", "w") as inventory:
+    # Writing data to a file
+    inventory.write("[controllers]\n")
+    for key, value in hosts.items():
+        match = re.search(r'^controller-\d', key)
+        if match:
+            inventory.write(value + '\n')
+    inventory.write("[workers]\n")
+    for key, value in hosts.items():
+        match = re.search(r'^worker-\d', key)
+        if match:
+            inventory.write(value + '\n')
